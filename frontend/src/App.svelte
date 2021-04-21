adcfe190d66fa72122d3a8493e7a9220765c2959
broeld <dinaiovcheva@gmail.com>
2021-04-21 18:02:59 +0300

    fear: front update

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,21 @@
 <script>
-	export let name;
+    import {Route, Router} from 'svelte-routing';
+    import Home from './Home.svelte'
+    import Auth from './auth/Auth.svelte'
+    import Redirect from './components/Redirect.svelte'
+
+    import { sessionEmail } from "./stores/auth";
+
+    $: isAuth = $sessionEmail;
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Router>
+    {#if !isAuth || typeof isAuth === undefined}
+        <Route path="login/"><Auth/></Route>
+        <Route path="*"><Redirect to="login"/></Route>
+    {:else}
+        <Route path="/"><Home/></Route>
+    {/if}
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+</Router>
